179ca71a12f479a39af5ecc801036b68ab79c5c4
Salim Kaboura <salimkaboura8@gmail.com>
2021-04-08 20:43:03 +0200

    Trying to add card animation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-import Portfolio from "./components/Portfolio";
+import { Portfolio } from "./components/Portfolio";
 import Services from "./components/Services";
 import Aboutme from "./components/Aboutme";
 import { BrowserRouter as Router, Route } from "react-router-dom";
