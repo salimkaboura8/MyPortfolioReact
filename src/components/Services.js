87d50951c1da5b42b610229e3fc591aac7656102
Salim Kaboura <salimkaboura8@gmail.com>
2021-04-10 10:50:29 +0200

    Services page aligned

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,41 +6,41 @@ import paintingLogo from "../images/paint.png";
 
 const Services = () => {
   return (
-    <div class="services-wrapper container">
+    <div class="services-wrapper">
       <div class="row">
-        <div class="col-sm">
-          <h1 className="textone xs">My Services</h1>
+        <div class="col-sm-4">
+          <h1 className="textone">My Services</h1>
           <p className="texttwo">
             With skills in over 4 different fields of design, I am the perfect
             person to hire when it comes to a full fledged project. Whatever
             your needs are, I can pretty much take on any challenge.
           </p>
         </div>
-        <div class="col-sm">
-          <img class="laptopLogo xs" src={laptopLogo} />
+        <div class="col-sm-4">
+          <img class="myimg laptopLogo" src={laptopLogo} />
           <h1 className="textonesm">Website Development</h1>
           <p className="texttwosm">
             With skills in over 4 different fields of design, I am the perfect
             person to hire when it comes to a full fledged project. Whatever
             your needs are, I can pretty much take on any challenge.
           </p>
-          <img class="laptopLogo" height="20%" src={mobileLogo} />
-          <h1 className="textonesm">Mobile Development</h1>
-          <p className="texttwosm">
-            With skills in over 4 different fields of design, I am the perfect
-            person to hire when it comes to a full fledged project. Whatever
-            your needs are, I can pretty much take on any challenge.
-          </p>
-        </div>
-        <div class="col-sm">
-          <img class="laptopLogo" height="20%" src={paintingLogo} />
+          <img class="myimg paintingLogo" src={paintingLogo} />
           <h1 className="textonesm">Graphic Design</h1>
           <p className="texttwosm">
             With skills in over 4 different fields of design, I am the perfect
             person to hire when it comes to a full fledged project. Whatever
             your needs are, I can pretty much take on any challenge.
           </p>
-          <img class="laptopLogo" height="20%" src={logoLogo} />
+        </div>
+        <div class="col-sm-4">
+          <img class="myimg mobileLogo" src={mobileLogo} />
+          <h1 className="textonesm">Mobile Development</h1>
+          <p className="texttwosm">
+            With skills in over 4 different fields of design, I am the perfect
+            person to hire when it comes to a full fledged project. Whatever
+            your needs are, I can pretty much take on any challenge.
+          </p>
+          <img class="myimg logoLogo" src={logoLogo} />
           <h1 className="textonesm">Logo Design</h1>
           <p className="texttwosm">
             With skills in over 4 different fields of design, I am the perfect
@@ -50,6 +50,17 @@ const Services = () => {
         </div>
       </div>
     </div>
+
+    /*<div class="container">
+      <div class="row">
+        <div class="col">
+          
+        <div class="col">
+          
+          
+        </div>
+      </div>
+    </div>*/
   );
 };
 
